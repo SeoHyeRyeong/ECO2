157174d9bdc8910625c74653bc0586b55c5cd99b
YURIM LEE <yur510@naver.com>
2022-07-23 17:30:44 +0900

    투두 추가

---
+++ app/src/main/java/com/example/eco2/toDo.kt
@@ -0,0 +1,11 @@
+package com.example.eco2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class toDo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_to_do)
+    }
+}
