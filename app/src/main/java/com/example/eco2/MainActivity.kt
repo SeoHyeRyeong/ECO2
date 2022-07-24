62dd9d30e0a8b688cd893c258d6a6f5439b502e4
YURIM LEE <yur510@naver.com>
2022-07-24 18:29:44 +0900

    1. todo, home, news 위치 수정 2. todo 버튼 화면 전환 구현

--- app/src/main/java/com/example/eco2/MainActivity.kt
+++ app/src/main/java/com/example/eco2/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.eco2
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,10 +9,20 @@ import android.widget.Button
 import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
-
+    
+    lateinit var todoBtn: ImageButton
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        
+        todoBtn = findViewById(R.id.todoButton)
+        
+        todoBtn.setOnClickListener{
+            var intent = Intent(this, toDo::class.java)
+            startActivity(intent)
+        }
     }
+    
+
 }
