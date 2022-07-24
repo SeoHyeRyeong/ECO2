15180cb25fc983821c0cf028f8781e81a337037d
YURIM LEE <yur510@naver.com>
2022-07-24 17:58:04 +0900

    - todo, home, news 버튼 추가 - color 추가

--- app/src/main/java/com/example/eco2/MainActivity.kt
+++ app/src/main/java/com/example/eco2/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.eco2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
