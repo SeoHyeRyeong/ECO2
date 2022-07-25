f57348e0d5be2dcad0e56ad878e81c455430044e
YURIM LEE <yur510@naver.com>
2022-07-25 14:55:26 +0900

    폰트 설정, 투두 화면 수정

--- app/src/main/java/com/example/eco2/todoActivity.kt
+++ app/src/main/java/com/example/eco2/todoActivity.kt
@@ -3,6 +3,7 @@ package com.example.eco2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 
 class todoActivity : AppCompatActivity() {
@@ -10,12 +11,16 @@ class todoActivity : AppCompatActivity() {
     lateinit var homeBtn: ImageButton
     lateinit var newsBtn: ImageButton
 
+    lateinit var listBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_to_do)
 
         homeBtn = findViewById(R.id.homeButton)
         newsBtn = findViewById(R.id.newsButton)
+
+        listBtn = findViewById(R.id.listButton)
 
         homeBtn.setOnClickListener{
             var intent = Intent(this, MainActivity::class.java)
