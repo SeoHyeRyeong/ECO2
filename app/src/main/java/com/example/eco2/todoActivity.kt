44b84990d679ca63483d6b367ae0d7eed0b1e222
YURIM LEE <yur510@naver.com>
2022-07-25 15:38:14 +0900

    폰트명 수정, 투두 화면 수정

--- app/src/main/java/com/example/eco2/todoActivity.kt
+++ app/src/main/java/com/example/eco2/todoActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class todoActivity : AppCompatActivity() {
 
@@ -12,6 +15,10 @@ class todoActivity : AppCompatActivity() {
     lateinit var newsBtn: ImageButton
 
     lateinit var listBtn: Button
+    lateinit var dateText: TextView
+
+    lateinit var simpleDateFormat: SimpleDateFormat
+    lateinit var date: Date
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +28,25 @@ class todoActivity : AppCompatActivity() {
         newsBtn = findViewById(R.id.newsButton)
 
         listBtn = findViewById(R.id.listButton)
+        dateText = findViewById(R.id.dateText)
 
+        val now: Long = System.currentTimeMillis()
+        date = Date(now)
+        simpleDateFormat = SimpleDateFormat("yyyy. MM. dd (E)") //형식 지정
+        val getDate: String = simpleDateFormat.format(date)
+        dateText.setText(getDate)
+
+        //홈 화면 연결
         homeBtn.setOnClickListener{
             var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-
+        //뉴스 화면 연결
         newsBtn.setOnClickListener{
             var intent = Intent(this, newsActivity::class.java)
             startActivity(intent)
         }
+
+
     }
 }
