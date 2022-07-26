7a73931e9cb718dd99830cdee42c9aec621da1d1
yoonjisoo <qwer18155181@gmail.com>
2022-07-26 14:31:37 +0900

    코드 작성

---
+++ app/src/main/java/com/example/eco2/MyAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.eco2
+
+import java.util.ArrayList
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class MyAdapter(val items: ArrayList<newsActivity.Item>, context: Context) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
+
+
+    //뷰홀더
+    inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+         var tv_title: TextView = view.findViewById(R.id.tv_title)
+         var tv_summary: TextView = view.findViewById(R.id.tv_summary)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+        return ViewHolder(v)
+    }
+
+    //아이템 개수 변환
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    //position번째 데이터와 xml 연결
+    //질문- 왜
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tv_title?.text = items.get(position).title
+        val into = Glide.with(holder.view.context)
+            .load(items.get(position).thumb)
+            .fitCenter()
+            .into(holder.view.findViewById(R.id.imageView))
+        holder.tv_summary.text = items.get(position).summary
+
+        //Click Event
+        holder.itemView.setOnClickListener {
+            val openUrl = Intent(Intent.ACTION_VIEW)
+            openUrl.data = Uri.parse(items.get(position).link)
+            startActivity(holder.view.context, openUrl, null)
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/eco2/newsActivity.kt
+++ app/src/main/java/com/example/eco2/newsActivity.kt
@@ -1,14 +1,30 @@
 package com.example.eco2
 
+import android.content.ClipData
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import org.jsoup.Jsoup
+import java.util.*
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.IOException
 
 class newsActivity : AppCompatActivity() {
 
     lateinit var homeBtn: ImageButton
     lateinit var todoBtn: ImageButton
+    lateinit var refreshBtn: ImageButton
+
+    lateinit var recyclerView:RecyclerView
+
+    var items : ArrayList<Item> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,15 +32,54 @@ class newsActivity : AppCompatActivity() {
 
         homeBtn = findViewById(R.id.homeButton)
         todoBtn = findViewById(R.id.todoButton)
+        refreshBtn = findViewById(R.id.refreshBtn)
 
-        homeBtn.setOnClickListener{
+        recyclerView = findViewById(R.id.recyclerView)
+
+        homeBtn.setOnClickListener {
             var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
-        todoBtn.setOnClickListener{
+        todoBtn.setOnClickListener {
             var intent = Intent(this, todoActivity::class.java)
             startActivity(intent)
         }
+
+
+        refreshBtn.setOnClickListener {
+            Thread(Runnable {
+                val url = "https://sports.news.naver.com/index.nhn"
+                val base_url = "https://sports.news.naver.com/"
+                val doc = Jsoup.connect(url).get()
+                val title = doc.title()
+                //val links = doc.select("a[href]")
+                //필요한 자료만 - 오늘의 스포츠 NoW 목록을 가져오자. HTML  태그등을 분석해서 무엇을 가져 올지 결정해야 한다.
+                val today = doc.select("ul.today_list li.today_item")
+
+                today.forEach { item ->
+                    val item_link = base_url + item.select("a").attr("href")
+                    val item_title = item.select("strong.title").text()
+                    val item_thumb = item.select("img").attr("src")
+                    val item_summary = item.select("p.news").text()
+
+                    //arrayList 리스트에 추가해 준다.
+                    items.add(Item(item_title, item_link, item_thumb, item_summary))
+                }
+
+
+                this@newsActivity.runOnUiThread(java.lang.Runnable {
+                    //어답터 연결하기
+                    recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+                    var adapter = MyAdapter(items, this)
+                    recyclerView.adapter = adapter
+                })
+            }).start()
     }
+    }
+    //테이터 객체(제목/기사링크/썸네일이미지)
+    data class Item(val title: String, val link: String, val thumb: String, val summary: String)
+
+    val clientId = "zVorJaB67x3RJ1fWnllZ"
+    val clientSecret = "7jJ1jkb5pA"
 }
