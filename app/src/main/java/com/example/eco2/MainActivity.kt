45cdb94049b522c4f506c745f21cf0ad81cc14d5
SeoHyeRyeong <94300234+SeoHyeRyeong@users.noreply.github.com>
2022-07-31 17:55:22 +0900

    main 수정

--- app/src/main/java/com/example/eco2/MainActivity.kt
+++ app/src/main/java/com/example/eco2/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.eco2
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Calendar
+import java.util.Date
+import java.util.GregorianCalendar
+
 
 class MainActivity : AppCompatActivity() {
     
     lateinit var todoBtn: ImageButton
     lateinit var newsBtn: ImageButton
+
+    var countDownTimer: CountDownTimer? = null
+    var tv_timer: TextView? = null
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         
         todoBtn = findViewById(R.id.todoButton)
         newsBtn = findViewById(R.id.newsButton)
+        tv_timer = findViewById(R.id.tv_timer)
         
         todoBtn.setOnClickListener{
             var intent = Intent(this, todoActivity::class.java)
@@ -26,7 +36,44 @@ class MainActivity : AppCompatActivity() {
             var intent = Intent(this, newsActivity::class.java)
             startActivity(intent)
         }
+
+        countDownTimer = object : CountDownTimer(200000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val timer=tv_timer
+                timer?.setText(getTime())
+            }
+
+            override fun onFinish() {}
+        }
+        val count=countDownTimer
+        count?.start()
     }
-    
+
+
+    private fun getTime(): String? {
+        val date = Date()
+        val calendar: Calendar = GregorianCalendar()
+        calendar.time = date
+        val c_year = calendar[Calendar.YEAR]
+        val c_month = calendar[Calendar.MONTH]
+        val c_day = calendar[Calendar.DAY_OF_MONTH]
+        val c_hour = calendar[Calendar.HOUR_OF_DAY]
+        val c_min = calendar[Calendar.MINUTE]
+        val c_sec = calendar[Calendar.SECOND]
+        val baseCal: Calendar = GregorianCalendar(c_year, c_month, c_day, c_hour, c_min, c_sec)
+        val targetCal: Calendar = GregorianCalendar(2029, 6, 24, 12, 36, 56) //비교대상날짜
+        val diffSec = (targetCal.timeInMillis - baseCal.timeInMillis) / 1000
+        val diffDays = diffSec / (24 * 60 * 60)
+        targetCal.add(Calendar.DAY_OF_MONTH, (-diffDays).toInt())
+        val dayTime = Math.floor((diffDays).toDouble()).toInt()
+        val hourTime = Math.floor((diffSec / 3600-(dayTime*24)).toDouble()).toInt()
+        val minTime = Math.floor(((diffSec - (3600 * hourTime)) / 60).toDouble()).toInt()
+        val secTime = Math.floor((diffSec - 3600 * hourTime - 60 * minTime).toDouble()).toInt()
+        val day = String.format("%02d", dayTime)
+        val hour = String.format("%02d", hourTime)
+        val min = String.format("%02d", minTime)
+        val sec = String.format("%02d", secTime)
+        return "1.5℃ 오르기까지\n" + day + "일 " + hour + "시간 " + min + "분 " + sec + "초 남았습니다."
+    }
 
 }
